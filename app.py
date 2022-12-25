3c7e8d66dbcd5aaaff29502c0c651f97ab37f928
githubrepo88 <119106388+githubrepo88@users.noreply.github.com>
2022-12-25 23:02:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def getstream():  # put application's code here
         r = requests.post("https://lite-1x163215.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
+            return veri
             veri = re.findall('"URL":"(.*?)"',veri)
             veri = veri[0].replace("\/", "/")
             veri = veri.replace('edge3','edge10')
