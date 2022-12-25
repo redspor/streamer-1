56d57b3ae3c71148398023a3fa14a906d2969cf8
githubrepo88 <119106388+githubrepo88@users.noreply.github.com>
2022-12-25 22:56:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 from flask import Flask
 from flask import request
-import regex
+import re
 app = Flask(__name__)
 
 @app.route('/getstream',methods=['GET'])
@@ -10,20 +10,21 @@ def getstream():  # put application's code here
     if param == "getts":
         source = request.url
         source = source.replace('https://streamer.herokuapp.com/getstream?param=getts&source=','')
+        source = source.replace('http://127.0.0.1:5000/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'tr-TR,tr;q=0.9',
+            'origin': 'https://lite-1x163215.top',
+            'referer': 'https://lite-1x163215.top/',
             'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
         ts = requests.get(source,headers=headers)
@@ -34,7 +35,7 @@ def getstream():  # put application's code here
         r = requests.post("https://lite-1x163215.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
-            veri = regex.findall('"URL":"(.*?)"',veri)
+            veri = re.findall('"URL":"(.*?)"',veri)
             veri = veri[0].replace("\/", "/")
             veri = veri.replace('edge3','edge10')
             veri = veri.replace('edge4','edge10')
@@ -44,6 +45,8 @@ def getstream():  # put application's code here
                     "accept": "*/*",
                     "accept-encoding": "gzip, deflate, br",
                     "accept-language": "tr-TR, tr;q = 0.9",
+                    "origin": "https://lite-1x163215.top",
+                    "referer": "https://lite-1x163215.top/",
                     'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
                     'sec-ch-ua-mobile': '?0',
                     'sec-ch-ua-platform': '"Windows"',
@@ -56,11 +59,10 @@ def getstream():  # put application's code here
                 anadomain = anadomain[0]
                 tsal = requests.get(veri,headers=headers)
                 tsal = tsal.text.replace(videoid,anadomain+videoid)
-                '''tsal = tsal.replace('https://','https://streamer-herokuapp.com/getstream?param=getts&source=https://')'''
+                tsal = tsal.replace('https://','https://streamer.herokuapp.com/getstream?param=getts&source=https://')
                 return tsal
         else:
             return "Veri yok"
 
 if __name__ == '__main__':
     app.run()
-
