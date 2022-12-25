16c2985682a15ecd3eb69ca24308bbd5bf810af0
githubrepo88 <119106388+githubrepo88@users.noreply.github.com>
2022-12-25 23:47:32 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,13 +4,38 @@ from flask import request
 import re
 app = Flask(__name__)
 
+@app.route('/getm3u8',methods=['GET'])
+def getm3u8():
+    source = request.url
+    source = source.replace('https://streamer.herokuapp.com/getm3u8?source=', '')
+    source = source.replace('%2F', '/')
+    source = source.replace('%3F', '?')
+    videoid = request.args.get("videoid")
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "tr-TR, tr;q = 0.9",
+        "origin": "https://lite-1x163215.top",
+        "referer": "https://lite-1x163215.top/",
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    }
+    ts = requests.get(source, headers=headers)
+    tsal = ts.text
+    tsal = tsal.replace(videoid+'_','https://streamer.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    return tsal
+
 @app.route('/getstream',methods=['GET'])
-def getstream():  # put application's code here
+def getstream():
     param = request.args.get("param")
     if param == "getts":
         source = request.url
         source = source.replace('https://streamer.herokuapp.com/getstream?param=getts&source=','')
-        source = source.replace('http://127.0.0.1:5000/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
@@ -35,33 +60,13 @@ def getstream():  # put application's code here
         r = requests.post("https://lite-1x163215.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
-            return veri
             veri = re.findall('"URL":"(.*?)"',veri)
             veri = veri[0].replace("\/", "/")
             veri = veri.replace('edge3','edge10')
             veri = veri.replace('edge4','edge10')
             veri = veri.replace('edge2','edge10')
             if "m3u8" in veri:
-                headers = {
-                    "accept": "*/*",
-                    "accept-encoding": "gzip, deflate, br",
-                    "accept-language": "tr-TR, tr;q = 0.9",
-                    "origin": "https://lite-1x163215.top",
-                    "referer": "https://lite-1x163215.top/",
-                    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'empty',
-                    'sec-fetch-mode': 'cors',
-                    'sec-fetch-site': 'cross-site',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-                }
-                anadomain = veri.split("mediaplaylist")
-                anadomain = anadomain[0]
-                '''tsal = requests.get(veri,headers=headers)
-                tsal = tsal.text.replace(videoid,anadomain+videoid)'''
-                tsal = tsal.replace('https://','https://streamer.herokuapp.com/getstream?param=getts&source=https://')
-                return tsal
+                return "https://streamer.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid
         else:
             return "Veri yok"
 
