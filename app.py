c7722963440747e60c566567d4c41c81f6946358
redspor <121644986+redspor@users.noreply.github.com>
2023-01-01 14:36:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,55 +2,28 @@ import requests
 from flask import Flask
 from flask import request
 import re
-from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
-@app.route('/getm3u8',methods=['GET'])
-def getm3u8():
-    source = request.url
-    source = source.replace('https://1xstreamer.azurewebsites.net/getm3u8?source=', '')
-    source = source.replace('%2F', '/')
-    source = source.replace('%3F', '?')
-    videoid = request.args.get("videoid")
-    headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-    }
-    ts = requests.get("http://1xstreamer.azurewebsites.net/getm3u8?source=https://edge10.xmediaget.com/hls-live/9510503/1/mediaplaylist.m3u8?s=a2f1854d23e4986571dd0883495a1580ffe58d4643e9ebe61e92bfe32a8df994&t=1672274057047&ai=3&av=1025&ui=0", headers=headers)
-    tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://1xstreamer.azurewebsites.net/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
-    return tsal
 
 @app.route('/getstream',methods=['GET'])
-def getstream():
+def getstream():  # put application's code here
     param = request.args.get("param")
     if param == "getts":
         source = request.url
-        source = source.replace('https://1xstreamer.azurewebsites.net/getstream?param=getts&source=','')
+        source = source.replace('https://workgeme.herokuapp.com/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
-            'accept': '*/*',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'tr-TR,tr;q=0.9',
-            'origin': 'https://www.maltinok.com',
-            'referer': 'https://www.maltinok.com/',
             'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
         ts = requests.get(source,headers=headers)
@@ -66,12 +39,27 @@ def getstream():
             veri = veri.replace('edge3','edge10')
             veri = veri.replace('edge4','edge10')
             veri = veri.replace('edge2','edge10')
-            veri = veri.replace(':43434','')
             if "m3u8" in veri:
-                return "https://1xstreamer.azurewebsites.net/getm3u8?source="+veri+'&videoid='+videoid
+                headers = {
+                    "accept": "*/*",
+                    "accept-encoding": "gzip, deflate, br",
+                    "accept-language": "tr-TR, tr;q = 0.9",
+                    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'sec-fetch-dest': 'empty',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-site': 'cross-site',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+                }
+                anadomain = veri.split("mediaplaylist")
+                anadomain = anadomain[0]
+                tsal = requests.get(veri,headers=headers)
+                tsal = tsal.text.replace(videoid,anadomain+videoid)
+                '''tsal = tsal.replace('https://workgeme.herokuapp.com/getstream?param=getts&source=https://')'''
+                return tsal
         else:
             return "Veri yok"
 
 if __name__ == '__main__':
     app.run()
-
